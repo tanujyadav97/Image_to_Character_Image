f2fd57757c00cde09ccf1384aba50d1307f2cb59
Tanuj <tanujyadav171997@gmail.com>
2018-11-05 03:21:37 +0530

    formatted code

--- converter.py
+++ converter.py
@@ -5,7 +5,8 @@ from PIL import Image
 invert_color = 0
 
 # these the characters used to create the image, grey intensity increases towards right
-point = ['`','.',',','^',':','-','~','+','*','?','@','%','#','$']
+point = ['`', '.', ',', '^', ':', '-', '~', '+', '*', '?', '@', '%', '#', '$']
+
 
 def scaleImage(image):
     if image.size[0] > image.size[1]:
@@ -19,30 +20,32 @@ def scaleImage(image):
 
     return image
 
-# keep your image path here
-image = Image.open("original-images/boycart.jpg", "r")
 
-# converting rgb image to grey-scale
-image = image.convert('L')
+if __name__ == '__main__':
+    # keep your image path here
+    image = Image.open("original-images/boycart.jpg", "r")
 
-# scaling image to smaller size
-scaledimage = scaleImage(image)
+    # converting rgb image to grey-scale
+    image = image.convert('L')
 
-# converting image to a matrix of grey values
-mat=np.asarray(scaledimage.getdata(),dtype=np.int16).reshape((scaledimage.size[1],scaledimage.size[0]))
+    # scaling image to smaller size
+    scaledimage = scaleImage(image)
 
-print(mat)
+    # converting image to a matrix of grey values
+    mat = np.asarray(scaledimage.getdata(), dtype=np.int16).reshape((scaledimage.size[1], scaledimage.size[0]))
 
-# this string contains the new image
-newimage = ""
+    print(mat)
 
-for i in range(0,mat.shape[0]):
-    for j in range(0,mat.shape[1]):
-        if invert_color:
-            newimage += (point[int( mat[i][j] / 19)])
-        else:
-            newimage+=(point[int((255-mat[i][j])/19)])
-    newimage+='\n'
+    # this string contains the new image
+    newimage = ""
 
-# print the new image to console
-print(newimage)
+    for i in range(0, mat.shape[0]):
+        for j in range(0, mat.shape[1]):
+            if invert_color:
+                newimage += (point[int(mat[i][j] / 19)])
+            else:
+                newimage += (point[int((255 - mat[i][j]) / 19)])
+        newimage += '\n'
+
+    # print the new image to console
+    print(newimage)
