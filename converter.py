7cddeeb478d20e6e9a18943af1bb5331566a00eb
Tanuj <tanujyadav171997@gmail.com>
2018-11-10 02:48:05 +0530

    solved the image width problem and added choice to resize the image

--- converter.py
+++ converter.py
@@ -4,26 +4,28 @@ from PIL import Image
 # Change this to 1 if you want to invert the grey-scale values
 invert_color = 0
 
+# Change this to 0 if you don't want to resize the image before conversion
+resize = 1
+
 # these the characters used to create the image, grey intensity increases towards right
 point = ['`', '.', ',', '^', ':', '-', '~', '+', '*', '?', '@', '%', '#', '$']
 
-
 def scaleImage(image):
-    if image.size[0] > image.size[1]:
-        basewidth = 150
-        height = (int)((basewidth / image.size[0]) * image.size[1])
-        image = image.resize((basewidth, height), Image.ANTIALIAS)
-    else:
-        height = 90
-        basewidth = (int)((height / image.size[1]) * image.size[0])
-        image = image.resize((basewidth, height), Image.ANTIALIAS)
+    if resize:
+        if image.size[0] > image.size[1]:
+            basewidth = 150
+            height = (int)((basewidth / image.size[0]) * image.size[1])
+            image = image.resize((basewidth, height), Image.ANTIALIAS)
+        else:
+            height = 90
+            basewidth = (int)((height / image.size[1]) * image.size[0])
+            image = image.resize((basewidth, height), Image.ANTIALIAS)
 
     return image
 
-
 if __name__ == '__main__':
     # keep your image path here
-    image = Image.open("original-images/boycart.jpg", "r")
+    image = Image.open("original-images/boy.jpg", "r")
 
     # converting rgb image to grey-scale
     image = image.convert('L')
@@ -42,9 +44,9 @@ if __name__ == '__main__':
     for i in range(0, mat.shape[0]):
         for j in range(0, mat.shape[1]):
             if invert_color:
-                newimage += (point[int(mat[i][j] / 19)])
+                newimage += (point[int(mat[i][j] / 19)])+(point[int(mat[i][j] / 19)])
             else:
-                newimage += (point[int((255 - mat[i][j]) / 19)])
+                newimage += (point[int((255 - mat[i][j]) / 19)])+(point[int((255 - mat[i][j]) / 19)])
         newimage += '\n'
 
     # print the new image to console
